1b50ecfc661788ad59f40011899c44578c7c13f5
Dennis Maina <denniskamwene1@gmail.com>
2023-09-26 09:13:45 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+// Collection is an argument that represents an array of objects
+function superbowlWin(collection) {
+    const winRecord = collection.find(record => record.result === "W");
+    return winRecord ? winRecord.year : undefined;
+  }
+  
